670d410f4996d744c95a024c0de6f4827e07e17b
George Rejoice <georgerejoice20@gmail.com>
2025-01-11 13:53:26 +0100

    added the input function feature

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from reji import get_marves_info
+name: str = input('Enter a name:  ')
+age: str = input('Enter a age:  ')
+gender: str = input('Enter a gender: ')
+state: str = input('Enter a state of origin:  ')
+food: str = input('Enter your faviourite food:  ')
 
 
-def hello(name: str) -> str:
-    return f"Hello {name}!"
+story: str = f"""
+______________________________________________________
+Hi, i'm {name} , {age} years old, a {gender},
+i'm from {state} state ,
+which am proud to be from,
+my favourite food is {food}.
+THANKYOU.
+________________________________________________________"""
 
-
-print(hello('rejoice'))
-
-
-def add(a: int, b: int) -> int:
-    return a + b
-
-
-print(add(1, 2))
-
-print(get_marves_info())
+print(story)
